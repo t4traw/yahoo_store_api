109541e2a711cd32fde7f97a112b54b7ee1f9ff5
Tatsuro Moriyama <t-moriyama@tom-cat.co.jp>
2017-05-10 16:36:44 +0900

    💇

--- lib/yahoo_store_api/stock/get_stock.rb
+++ lib/yahoo_store_api/stock/get_stock.rb
@@ -3,7 +3,9 @@ module YahooStoreApi
     include YahooStoreApi::Helper
 
     def get_stock(item_code)
-      handler connection('getStock').post {|r| r.body = "seller_id=#{@seller_id}&item_code=#{item_code}"}
+      handler connection('getStock').post {
+        |r| r.body = "seller_id=#{@seller_id}&item_code=#{item_code}"
+      }
     end
 
   end
