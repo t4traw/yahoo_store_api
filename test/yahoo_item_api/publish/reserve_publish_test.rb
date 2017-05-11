6c8528e68f5dfdd9325bb355721bd000f50a1fa3
t4traw <t4traw@gmail.com>
2017-05-11 22:25:38 +0900

    reserve_publishのテスト追加

---
+++ test/yahoo_item_api/publish/reserve_publish_test.rb
@@ -0,0 +1,12 @@
+require 'yahoo_store_api'
+require 'test_helper'
+
+class YahooStoreApiTest < Minitest::Test
+  include TestHelper::Client
+
+  def test_reserve_publish
+    VCR.use_cassette('publish/reservePublish') do
+      assert_equal 'OK', client.reserve_publish.status
+    end
+  end
+end
